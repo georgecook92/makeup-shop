36c68277ac338e324ebf074ac7eabebc12c30840
George <george.cook.92@hotmail.co.uk>
2016-10-17 14:19:18 +0100

    change logic for checking if user exists in db already

--- api/model/authModel.js
+++ api/model/authModel.js
@@ -20,7 +20,7 @@ export default class AuthModel {
           //  how the mysql library is wrapped - stackoverflow
           connection.connection.release();
           this.res.status(200).json({
-            "success": true
+            success: true
           });
         } else {
           //  how the mysql library is wrapped - stackoverflow
@@ -35,67 +35,65 @@ export default class AuthModel {
   }
 
   register() {
-    pool.getConnection().then(connection => {
-      return connection.query(this.sql, [this.data.email]).then(result => {
-        if (result.length > 0) {
-          // works **********************************
-          throw new Error('Exists');
-        } else {
-          try {
-            bcrypt.genSalt(saltRounds, (err, salt) => {
-              if (err) {
-                throw new Error(err);
-              }
-              bcrypt.hash(this.data.password, salt, (err, hash) => {
-                if (err) {
-                  throw new Error(err);
-                }
-                this.data.password = hash;
-
-                let {email, password, first_name, last_name, phone} = this.data;
-
-                var user = {email, password, first_name, last_name, phone};
-
-                if (!email || !password || !first_name || !last_name || !phone) {
-                  // to do with the node-mysql library
-                  connection.connection.release();
-                  console.log('error - not all fields');
-                  // logs but does not throw - to do with nested callbacks? *************************************
-                  throw new Error('Provide All Fields');
-                } else {
-                  return pool.getConnection().then(conn => {
-                    return conn.query('INSERT INTO _users SET ?', user).then((result) => {
-                      var url = 'www.testsite.com';
-                      var registerEmailContent = 'You are receiving this because you (or someone else) have signed up ' +
-                      'to the website.\n\n Please click on the following link, or paste this into your browser to complete' +
-                       'the process:\n\n' + url + '/confirmEmail/token \n\n Once you have confirmed your account,' +
-                       ' you will be able to login.\n';
-                      var email = new Email(this.data.email, 'userconfirmation@makeup.com', 'Confirm Account', registerEmailContent, this.res);
-                      email.sendTokenEmail();
-                      conn.connection.release();
+    var {email, password, first_name, last_name, phone} = this.data;
+    var user = {email, password, first_name, last_name, phone};
+    try {
+      if (!email || !password || !first_name || !last_name || !phone) {
+        // to do with the node-mysql library
+        console.log('error - not all fields');
+        throw new Error('Provide All Fields');
+      } else {
+        pool.getConnection().then(connection => {
+          return connection.query(this.sql, [this.data.email]).then(result => {
+            if (result.length > 0) {
+              throw new Error('Exists');
+            } else {
+              try {
+                bcrypt.genSalt(saltRounds, (err, salt) => {
+                  if (err) {
+                    throw new Error(err);
+                  }
+                  bcrypt.hash(this.data.password, salt, (err, hash) => {
+                    if (err) {
+                      throw new Error(err);
+                    }
+                    this.data.password = hash;
+                    return pool.getConnection().then(conn => {
+                      return conn.query('INSERT INTO _users SET ?', user).then(result => {
+                        var url = 'www.testsite.com';
+                        var registerEmailContent = 'You are receiving this because you (or someone else) have signed up ' +
+                        'to the website.\n\n Please click on the following link, or paste this into your browser to complete' +
+                         'the process:\n\n' + url + '/confirmEmail/token \n\n Once you have confirmed your account,' +
+                         ' you will be able to login.\n';
+                        var email = new Email(this.data.email, 'userconfirmation@makeup.com', 'Confirm Account', registerEmailContent, this.res);
+                        email.sendTokenEmail();
+                        conn.connection.release();
+                      });
                     });
-                  });
-                }
+                  }
+                  );
+                });
+              } catch (e) {
+                console.log(e);
+                this.next(e);
               }
-              );
-            });
-          } catch (e) {
-            console.log(e);
-            this.next(e);
-          }
-        }
-      });
-    }).catch(err => {
-      console.log(err);
-      this.next(err);
-    });
+            }
+          });
+        }).catch(err => {
+          console.log(err);
+          this.next(err);
+        });
+      }
+    } catch (e) {
+      console.log(e);
+      this.next(e);
+    }
   }
 
   login() {
-    pool.getConnection().then((connection) => {
-      return connection.query(this.sql, [this.data.email]).then((result)=>{
+    pool.getConnection().then(connection => {
+      return connection.query(this.sql, [this.data.email]).then(result => {
         if (result.length > 0) {
-          console.log(result);
           // Load hash from your password DB.
           bcrypt.compare(this.data.password, result[0].password, (err, comparisonValue) => {
             try {
@@ -106,34 +104,28 @@ export default class AuthModel {
               }
 
               if (comparisonValue) {
-                console.log('correct.');
                 connection.connection.release();
                 console.log(result[0]);
                 this.res.status(200).json({
-                  "email" : result[0].email,
-                  "first_name" : result[0].first_name,
-                  "last_name" : result[0].last_name,
-                  "phone" : result[0].phone
-                 });
-
+                  email: result[0].email,
+                  first_name: result[0].first_name,
+                  last_name: result[0].last_name,
+                  phone: result[0].phone
+                });
               } else {
-                console.log('wrong.');
                 connection.connection.release();
                 throw new Error('Incorrect Password');
               }
-            } catch(e) {
+            } catch (e) {
               console.log(e);
               this.next(e);
             }
           });
-
         }
-      }).catch( (err) => {
+      }).catch(err => {
         console.log(err);
         this.next(err);
       });
-    })
-
+    });
   }
-
 }
--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,54 +1,45 @@
 var express = require('express');
 var router = express.Router();
-var mysql = require('mysql');
-
-
 import AuthModel from '../model/authModel.js';
-
-
 
 var RateLimit = require('express-rate-limit');
 
 var createAccountLimiter = new RateLimit({
-  windowMs: 5*60*1000, // 5 minute window
+  windowMs: 5 * 60 * 1000, // 5 minute window
   delayAfter: 3, // begin slowing down responses after the first request
-  delayMs: 3*1000, // slow down subsequent responses by 3 seconds per request
+  delayMs: 3 * 1000, // slow down subsequent responses by 3 seconds per request
   max: 5, // start blocking after 5 requests
   message: "Too many attempts from this IP, please try again in five minutes",
-  handler: (req,res) => {
-    res.json({ 'error': 'Too many attempts from this IP, please try again in five minutes' });
+  handler: (req, res) => {
+    res.json({error: 'Too many attempts from this IP, please try again in five minutes'});
   }
 });
 
-//TODO:
+// send email out on registration - spam control - CHANGE SEND GRID *************************
 
-//send email out on registration - spam control - CHANGE SEND GRID *************************
+// forgot password
+// change password (try and make this work for both (ie above as well))
 
-//forgot password
-//change password (try and make this work for both (ie above as well))
-
-router.post('/login', createAccountLimiter, (req,res,next) => {
+router.post('/login', createAccountLimiter, (req, res, next) => {
   var emailCheckSQL = 'select * from `_users` where `email` = ?';
   var Model = new AuthModel(emailCheckSQL, req.body, res, next);
   Model.login();
 });
 
-router.post('/register', createAccountLimiter, (req,res,next) => {
+router.post('/register', createAccountLimiter, (req, res, next) => {
   var emailCheckSQL = 'select * from `_users` where `email` = ?';
   var Model = new AuthModel(emailCheckSQL, req.body, res, next);
   Model.register();
 });
 
-router.post('/confirmUser', createAccountLimiter, (req,res,next) => {
-  let updateConfirmUserSQL = 'update _users set `verified` = 1, `token` = NULL where `token` = ?' ;
+router.post('/confirmUser', createAccountLimiter, (req, res, next) => {
+  var updateConfirmUserSQL = 'update _users set `verified` = 1, `token` = NULL where `token` = ?';
   var Model = new AuthModel(updateConfirmUserSQL, req.body, res, next);
   Model.confirmUser();
 });
 
-router.post('/changePassword', (req,res,next) => {
+router.post('/changePassword', (req, res, next) => {
 
 });
-
-
 
 module.exports = router;
