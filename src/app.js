193b9f80c55b341a8c4ebe45cfe3757a6c260364
George <george.cook.92@hotmail.co.uk>
2016-10-26 18:45:49 +0100

    adding react router

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import Home from './components/home.js';
+import Admin from './components/admin.js';
 
 import App from './components/app';
 import reducers from './reducers';
@@ -10,6 +13,14 @@ const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+
+    <Router history={browserHistory}>
+      <Route path='/' component={App}>
+        <IndexRoute component={Home} />
+        <Route path='admin' component={Admin} />
+      </Route>
+
+    </Router>
+
   </Provider>
   , document.querySelector('.container'));
